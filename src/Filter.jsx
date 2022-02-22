f8d4ca3dce20aa80268ffa319fe46f1a3c9374b6
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-22 14:00:22 +0100

    filter js

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,1 +1,24 @@
 import { useEffect } from "react";
+
+function Filter(props) {
+  useEffect(() => {
+    if (props.activeGenre === 0) {
+      props.setFiltered(props.popular);
+      return;
+    }
+    const filtered = props.popular.filter(movie =>
+      movie.genre_ids.includes(props.activeGenre)
+    );
+    props.setFiltered(filtered);
+  }, [props.activeGenre]);
+
+  return (
+    <div className='filter-container'>
+      <button onClick={() => props.setActiveGenre(0)}>All</button>
+      <button onClick={() => props.setActiveGenre(35)}>Comedy</button>
+      <button onClick={() => props.setActiveGenre(28)}>Action</button>
+    </div>
+  );
+}
+
+export default Filter;
