5c0953401df3f8c0ad8a705a7cb538f52ab9bb04
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-22 13:41:58 +0100

    filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from 'react';
+import Movie from "./Movie";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=183f6d81ebab47463edff434c4c7625b&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setPopular(movies.results);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="popular-movies">
+        { popular.map(movie => {
+          return <Movie key={movie.id} movie={movie} />
+        }) }
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
