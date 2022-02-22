467a14e6a240601043a75cc8725d816b1905d7da
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-22 15:44:07 +0100

    background texte

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -8,12 +8,18 @@ function Movie({ movie }) {
       exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 0.5 }}
       layout
+      className="movie__container"
     >
       <h2>{movie.title}</h2>
       <img
         src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
         alt={movie.title}
       />
+      <div className="wrapper">
+          <h3>{ movie.title }</h3>
+          <p>{ movie.overview }</p>
+          <strong>{ movie.release_date }</strong>
+      </div>
     </motion.div>
   );
 }
